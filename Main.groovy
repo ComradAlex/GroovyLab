e9d0c6e827ac272912ac32841ad345ce81b6a204
ComradAlex <55763012+ComradAlex@users.noreply.github.com>
2020-05-22 21:55:50 +0300

    Create Main.groovy

---
+++ Main.groovy
@@ -0,0 +1,18 @@
+//конструктор по умолчанию
+FirstClass firstClass = new FirstClass(1, "Name", "b@mail.ru", "designer")
+println firstClass
+Binding binding = new Binding()
+binding.setVariable("firstClass", new FirstClass(2, "str", "a@mail.ru", "developer"))
+println binding.getProperty("firstClass")
+println firstClass.getId()
+println firstClass.getName()
+println firstClass.getEmail()
+println firstClass.getPosition()
+
+def a = 5
+def b = "kek"
+println a + " " + b
+println String instanceof Integer
+
+Integer num = firstClass.calc(2)
+println num
